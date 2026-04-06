3a5499c14d44b75c3cbed5e97040f4447bf3bb1f
korapatiSreedhar <korapatisreedhar9999@gmail.com>
2026-04-06 12:11:46 +0530

    updated eveything now working fine

--- app.py
+++ app.py
@@ -2118,5 +2118,8 @@ def send_mail_route(email, status):
     except Exception as e:
         return f"<h3 style='color:red;'>Error: {str(e)}</h3>"
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
